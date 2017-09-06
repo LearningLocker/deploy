f3c9afcfb3c3b2114a3623915ab00405a65c4912
Gareth Ardron <gareth.ardron@ht2labs.com>
2017-09-06 10:51:57 +0100

    added no-clobber to cp of storage

--- llv2_build.sh
+++ llv2_build.sh
@@ -1499,8 +1499,8 @@ elif [[ $LOCAL_INSTALL == true ]] && [[ $UPDATE_MODE == true ]]; then
 
     # copy anything in the storage dirs over
     echo -n "[LL] Copying user uploaded data in storage/ folders to new install....."
-    cp ${SYMLINK_PATH}/storage/* ${LOCAL_PATH}/storage/
-    cp ${SYMLINK_PATH}/xapi/storage/* ${LOCAL_PATH}/xapi/storage/
+    cp -nR ${SYMLINK_PATH}/storage/* ${LOCAL_PATH}/storage/
+    cp -nR ${SYMLINK_PATH}/xapi/storage/* ${LOCAL_PATH}/xapi/storage/
     echo "done!"
 
     # prompt user that we're about to do the swap over
