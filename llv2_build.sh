9e1990ab39fad278f9e8bb338ef521b8540c1a8a
Gareth Ardron <gareth.ardron@ht2labs.com>
2017-09-28 14:13:17 +0100

    added noise supression to mongo install on debian/ubuntu

--- llv2_build.sh
+++ llv2_build.sh
@@ -704,7 +704,7 @@ function debian_mongo ()
     if [[ $OS_VERSION == "Ubuntu" ]]; then
         if [[ $OS_VNO == "16.04" ]]; then
             output "Setting up mongo repo (Stock Ubuntu version is too old)"
-            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+            apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 >> $OUTPUT_LOG 2>>$ERROR_LOG
             echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
             apt-get update >> $OUTPUT_LOG 2>>$ERROR_LOG
             systemctl unmask mongod
