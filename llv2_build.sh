08e95d9a0b1ca9ab009475200266051ca9212eb0
Gareth Ardron <gareth.ardron@ht2labs.com>
2017-11-29 14:17:17 +0000

    fix for typo on xapi config

--- llv2_build.sh
+++ llv2_build.sh
@@ -1638,7 +1638,7 @@ if [[ $ENTERPRISE == true ]]; then
     output "copying enterprise pm2 files"
     cp ${BUILDDIR}/${WEBAPP_SUBDIR}/pm2/worker.json.dist ${TMPDIR}/${WEBAPP_SUBDIR}/worker.json
     cp ${BUILDDIR}/${WEBAPP_SUBDIR}/pm2/webapp.json.dist ${TMPDIR}/${WEBAPP_SUBDIR}/webapp.json
-    cp ${BUILDDIR}/${XAPI_SUBDIR}/pm2/xapi.json.dist$ TMPDIR/${WEBAPP_SUBDIR}/xapi.json
+    cp ${BUILDDIR}/${XAPI_SUBDIR}/pm2/xapi.json.dist $TMPDIR/${WEBAPP_SUBDIR}/xapi.json
 else
     cp ${BUILDDIR}/${WEBAPP_SUBDIR}/pm2/all.json.dist ${TMPDIR}/${WEBAPP_SUBDIR}/all.json
     cp ${BUILDDIR}/${XAPI_SUBDIR}/pm2/xapi.json.dist $TMPDIR/${XAPI_SUBDIR}/xapi.json
